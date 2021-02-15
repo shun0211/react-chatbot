d92e4f0db2a133d140590df0d76a43673c1198f5
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-02-15 13:36:26 +0900

    part4完了

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import defaultDataset from './dataset';
+import './assets/styles/style.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentID: "init",
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+  render() {
+    return (
+      <section className="c-section">
+        <div className="c-box">
+          {this.state.currentID}
+        </div>
+      </section>
+    );
+  }
+}
