9254743702b8b913ddac48d066711e00facdedc3
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-02-16 09:52:21 +0900

    part6完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css';
+import {AnswersList, Chats} from './components/index';
+import { ThreeSixty } from '@material-ui/icons';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,12 +14,56 @@ export default class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this)
   }
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
+
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentID: 'nextQuestionId'
+    });
+  }
+
+  selectAnswer = (selectedAnswer, nextQuestionID) => {
+    switch(true) {
+      case (nextQuestionID === 'init'):
+        this.displayNextQuestion(nextQuestionID)
+
+        break;
+      default:
+        const chats = this.state.chats
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+
+        this.setState({
+          chats: chats
+        })
+
+        this.displayNextQuestion(nextQuestionID);
+        break;
+    }
+  }
+
+  componentDidMount() {
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentID)
+  }
+
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          {this.state.currentID}
+          <Chats chats={this.state.chats}/>
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
---
+++ src/components/Chat.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import NoProfile from '../assets/img/犬.jpg';
+import Sakai from '../assets/img/さかい.jpg';
+
+
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+
+  return(
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Sakai} />
+        ) : (
+          <Avatar alt="icon" src={NoProfile} />
+        )}
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{props.text}</div>
+    </ListItem>
+  )
+}
+
+export default Chat
